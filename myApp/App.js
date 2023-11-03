495ae834b68e6ff2fdfb5ade2d726ddbb5ae9788
Jorge L. Mejia Pejerrey <mejiapj@gmail.com>
2023-11-02 21:48:11 -0500

    Clase 9/16 -> Creación de componentes

--- myApp/App.js
+++ myApp/App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import LoginForm from './src/components/LoginForm';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hola DEVICE!</Text>
-      <StatusBar style="auto" />
+      <Text>Hola Mundo</Text>
+
+      <LoginForm></LoginForm>
     </View>
   );
 }
