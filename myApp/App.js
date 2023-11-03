f7fd0de7a60b37e85d039d73e42ee3e4aae3eb08
Jorge L. Mejia Pejerrey <mejiapj@gmail.com>
2023-11-02 22:01:52 -0500

    Clase 10/16 -> Propiedades entre componentes: props

--- myApp/App.js
+++ myApp/App.js
@@ -1,14 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import LoginForm from './src/components/LoginForm';
+import Saludar from './src/components/Saludar';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola DEVICE!</Text>
-      <Text>Hola Mundo</Text>
-
-      <LoginForm></LoginForm>
+      <Text>Curso React Native</Text>
+      <Saludar name="Agustin Navarro" />
+      <Saludar name="Carlos Navarro"></Saludar>
+      <Saludar name="Andres Navarro" />
     </View>
   );
 }
