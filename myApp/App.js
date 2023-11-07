e5fb7c6044f53321092a8bd4e748aca6e31677ff
Jorge L. Mejia Pejerrey <mejiapj@gmail.com>
2023-11-06 22:07:47 -0500

    Clase 12/16 -> Validación de propiedades: propTypes

--- myApp/App.js
+++ myApp/App.js
@@ -6,8 +6,8 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Curso React Native</Text>
-      <Saludar firstname="Agustin" lastname="Navarro Galdon" />
-      <Saludar />
+      <Saludar firstname="Agustin" lastname={28} />
+      <Saludar firstname="Agustin" lastname="Navarro" />
       {/*<Saludar name="Carlos Navarro"/>*/}
       {/*<Saludar name="Andres Navarro"/>*/}
     </View>
--- myApp/src/components/Saludar.js
+++ myApp/src/components/Saludar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Text } from 'react-native';
+import PropTypes from 'prop-types';
 
 export default function Saludar(props) {
   /* const { firstname = 'Agustin', lastname = 'Navarro' } = props; */
@@ -11,4 +12,9 @@ export default function Saludar(props) {
   );
 }
 
-Saludar.defaultProps = { firstname: 'Agustin', lastname: 'Navarro' };
+//Saludar.defaultProps = { firstname: 'Agustin', lastname: 'Navarro' };
+
+Saludar.propTypes = {
+  firstname: PropTypes.string.isRequired,
+  lastname: PropTypes.string.isRequired,
+};
