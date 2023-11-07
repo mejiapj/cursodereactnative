d2d5f3175b755a402b4656af3a2c303c1fdee6e9
Jorge L. Mejia Pejerrey <mejiapj@gmail.com>
2023-11-06 22:27:11 -0500

    Clase 13/16 -> Instalando y configurando React Navigation

--- myApp/App.js
+++ myApp/App.js
@@ -1,16 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
+import React from 'react';
+
+// import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
 import Saludar from './src/components/Saludar';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Curso React Native</Text>
-      <Saludar firstname="Agustin" lastname={28} />
-      <Saludar firstname="Agustin" lastname="Navarro" />
-      {/*<Saludar name="Carlos Navarro"/>*/}
-      {/*<Saludar name="Andres Navarro"/>*/}
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>Curso React Native</Text>
+        {/*       <Saludar firstname="Agustin" lastname="Navarro" /> */}
+        <Saludar firstname="Agustin" lastname="Navarro Galdon" />
+        {/*<Saludar name="Carlos Navarro"/>*/}
+        {/*<Saludar name="Andres Navarro"/>*/}
+      </View>
+    </NavigationContainer>
   );
 }
 
