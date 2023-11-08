31ccd8d0b6cbd68f7514dbc543b6eb273d8b99b2
Jorge L. Mejia Pejerrey <mejiapj@gmail.com>
2023-11-08 09:35:42 -0500

    Clase 16/16 -> Drawer navigation y próximos pasos

--- myApp/App.js
+++ myApp/App.js
@@ -1,24 +1,17 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-// import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import NavigationStack from './src/navigation/NavigationStack';
-
 import NavigationTab from './src/navigation/NavigationTab';
+// import NavigationDrawer from './src/navigation/NavigationDrawer';
 
 export default function App() {
   return (
     <NavigationContainer>
-      {/*       <View style={styles.container}>
-        <Text>Curso React Native</Text>
-        <Saludar firstname="Agustin" lastname="Navarro" />
-        <Saludar firstname="Agustin" lastname="Navarro Galdon" />
-        <Saludar name="Carlos Navarro" />
-        <Saludar name="Andres Navarro" />
-      </View> */}
-      {/*       <NavigationStack /> */}
+      {/* <NavigationStack /> */}
       <NavigationTab />
+      {/*    <NavigationDrawer /> */}
     </NavigationContainer>
   );
 }
